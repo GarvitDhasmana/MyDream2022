24b959c413713d77ccea5a767b47e95c89ca68cc
GarvitDhasmana <garvitdhasmana@gmail.com>
2022-08-17 20:37:51 +0530

    Add a new car class

---
+++ DreamJob/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamJob
+{
+    internal class Car
+    {
+        public int CarName { get; set; }
+    }
+}
