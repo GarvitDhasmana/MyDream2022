ecb6578842c93508038f01e1ff4ea1e7d88dade4
dines <dines@LAPTOP-KORN5E5D>
2022-08-17 20:14:16 +0530

    Add project files.

---
+++ DreamJob/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamJob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World");
+            //Console.WriteLine("My name is Garvit");
+            //Console.ReadLine();
+
+            // Sum function
+
+            //int firstvalue = 10;
+            //int secondvalue = 20;
+
+            //int result = firstvalue + secondvalue;
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+            // Console.WriteLine("Enter your first number");
+            // int first = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Enter your second number");
+            // int second = Convert.ToInt32(Console.ReadLine());
+
+            //int sum = first + second;
+            //int sub = first - second;
+            // int mul = first * second;
+            // int div = first / second;
+            // int mod = first % second;
+
+            // Console.WriteLine(sum);
+            // Console.WriteLine(div);
+            // Console.WriteLine(sub);
+            // Console.WriteLine(mul);
+            // Console.WriteLine(mod);
+
+            // Console.ReadLine();
+
+            //Console.WriteLine("Enter you Name");
+            //string StudentName = Console.ReadLine();
+
+            //Console.WriteLine("Enter you maths number");
+            //int mathsNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter you english number");
+            //int englishNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter your science number");
+            //int scienceNumber = Convert.ToInt32(Console.ReadLine());
+
+            //int StudentMarks = mathsNumber + englishNumber + scienceNumber;
+            //int totalMarks = 300;
+            //int percentage = (StudentMarks * 100) / totalMarks;
+
+            //Console.WriteLine("Total Marks of student " + StudentName + " is " + StudentMarks);
+            //Console.WriteLine("Total percentage of student " + StudentName + " is " + percentage);
+
+            //Console.ReadLine();
+
+            Console.WriteLine("Enter you Name");
+            string StudentName = Console.ReadLine();
+
+            Console.WriteLine("Enter you maths number");
+            int mathsNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter you english number");
+            int engNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your hindi number");
+            int hindiNumber = Convert.ToInt32(Console.ReadLine());
+
+            int studentMarks = mathsNumber + engNumber + hindiNumber;
+            int totalMarks = 300;
+            int percentage = (studentMarks * 100) / totalMarks;
+
+            Console.WriteLine("Total marks of student " + StudentName  + " is " + studentMarks);
+            Console.WriteLine("Total percentage of student " + " is " +  percentage);
+            
+
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+}
